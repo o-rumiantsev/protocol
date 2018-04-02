baa666f906695df132a22556a081251f66de562e
mille-nium <dianaboloten.db@gmail.com>
2018-04-02 16:05:28 +0300

    RPC implementation

--- example/server.js
+++ example/server.js
@@ -2,7 +2,21 @@
 
 const mhp = require('../');
 
-const s = new mhp.Server();
+const app = new mhp.Application('test', {
+  math: {
+    add(a, b, callback) {
+      const res = a + b;
+      callback(null, res);
+    }
+  },
+  words: {
+    toUpper(word, callback) {
+      callback(null, word.toUpperCase());
+    }
+  }
+});
+
+const s = new mhp.Server([app]);
 
 s.listen(3000, 'localhost', () => {
   console.log('MHP srver bound');
